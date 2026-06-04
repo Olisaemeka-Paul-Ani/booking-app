e9622800b3a7c9415aaccb762a413b3a1f1a6571
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-06-03 18:06:27 -0700

    Fix data type for input handling

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("ConferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -18,7 +18,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// ask user for their name
 	fmt.Println(("Enter your first name: "))
 	fmt.Scan(&firstName)
@@ -32,6 +32,9 @@ func main() {
 	fmt.Println(("Enter no of tickets: "))
 	fmt.Scan(&userTickets)
 
-	userTickets = 2
+	remainingTickets = remainingTickets - userTickets
 	fmt.Printf("User %v booked %v tickets. \n", firstName, userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
 }
