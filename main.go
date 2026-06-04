2589a80f5b4e6c0e046733df7729a1d44ecda1b2
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-06-03 17:10:30 -0700

    Added input handling for credentials

--- main.go
+++ main.go
@@ -5,19 +5,33 @@ import (
 )
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("ConferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and, %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	// ask user for their name
+	fmt.Println(("Enter your first name: "))
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
+	fmt.Println(("Enter your last name: "))
+	fmt.Scan(&lastName)
+
+	fmt.Println(("Enter your email adress: "))
+	fmt.Scan(&email)
+
+	fmt.Println(("Enter no of tickets: "))
+	fmt.Scan(&userTickets)
+
 	userTickets = 2
-	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+	fmt.Printf("User %v booked %v tickets. \n", firstName, userTickets)
 }
