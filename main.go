ec828950913e46a3bd4b8ec46925a20df7fbce87
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-06-04 19:35:15 -0700

    Add functionality for name storage

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 	fmt.Printf("We have total of %v tickets and, %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	//var bookings [50]string
+
 	var firstName string
 	var lastName string
 	var email string
