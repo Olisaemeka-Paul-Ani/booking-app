00c35edec5d9df176c78f0972493ba37d4cdf623
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-06-20 23:30:44 -0700

    Added functionality for unlimited booking indertion

--- main.go
+++ main.go
@@ -8,44 +8,43 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-
-	fmt.Printf("ConferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and, %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask user for their name
+		fmt.Println(("Enter your first name: "))
+		fmt.Scan(&firstName)
 
-	bookings := []string{}
+		fmt.Println(("Enter your last name: "))
+		fmt.Scan(&lastName)
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	// ask user for their name
-	fmt.Println(("Enter your first name: "))
-	fmt.Scan(&firstName)
+		fmt.Println(("Enter your email adress: "))
+		fmt.Scan(&email)
 
-	fmt.Println(("Enter your last name: "))
-	fmt.Scan(&lastName)
+		fmt.Println(("Enter no of tickets: "))
+		fmt.Scan(&userTickets)
 
-	fmt.Println(("Enter your email adress: "))
-	fmt.Scan(&email)
+		remainingTickets = remainingTickets - userTickets
+		fmt.Printf("User %v booked %v tickets. \n", firstName, userTickets)
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Println(("Enter no of tickets: "))
-	fmt.Scan(&userTickets)
+		fmt.Printf("the whole slice: %v \n", bookings)
+		fmt.Printf("the first value: %v \n", bookings[0])
+		fmt.Printf("the type: %T \n", bookings)
+		fmt.Printf("the length: %v \n", len(bookings))
 
-	remainingTickets = remainingTickets - userTickets
-	fmt.Printf("User %v booked %v tickets. \n", firstName, userTickets)
-	bookings = append(bookings, firstName+" "+lastName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("the whole slice: %v \n", bookings)
-	fmt.Printf("the first value: %v \n", bookings[0])
-	fmt.Printf("the type: %T \n", bookings)
-	fmt.Printf("the length: %v \n", len(bookings))
+		fmt.Println("These are all our bookings: %v\n", bookings)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-	fmt.Println("These are all our bookings: %v\n", bookings)
+	}
 
 }
